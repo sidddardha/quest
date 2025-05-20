646d6bb2068715c5429f52afae2d561a1fd3fc8e
Siddardha Kotla <siddardha.kotla@siddardha.kotla-XC2WYCG2M7>
2025-05-20 21:09:45 +0530

    terraform code

---
+++ quest-terraform/outputs.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "docker_image" {
+  default = "siddardhakotla/quest-app:latest"
+}
+
+variable "app_name" {
+  default = "quest-app"
+}
